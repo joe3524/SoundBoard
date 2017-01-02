01053f5a0f53036ff2d405d22374c9d31f8d53b5
Joe K <joe@joe-me294khaui-MacBook-Pro.local>
2017-01-02 10:51:22 +0900

    Initial Commit

---
+++ Sound Board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sound Board
+//
+//  Created by joe_mac on 01/02/2017.
+//  Copyright © 2017 joe_mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
